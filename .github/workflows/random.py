d5c2b2f19255414c421274528210513fca459346
7in11 <124911362+7in11@users.noreply.github.com>
2023-02-09 21:51:28 +0500

    Create random.py

---
+++ .github/workflows/random.py
@@ -0,0 +1,24 @@
+from random import randint
+a=randint(1, 10)
+if a==1:
+    print('нормуль')
+elif a==2:
+    print('ok')
+elif a==3:
+    print('ахахахахахахаххахаха')    
+elif a==4:
+    print('бб')
+elif a==5:
+    print('ладно')
+elif a==6:
+    print('хахахах')        
+elif a==7:
+    print('лол')
+elif a==8:
+    print('смешно')
+elif a==9:
+    print('пон')
+elif a==10:
+    print('давай')    
+else:
+    print('err')
